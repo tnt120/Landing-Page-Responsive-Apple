a0b5c315708884455dede0d99d8e73f8ee77115a
tnt120 <apaj04@gmail.com>
2021-08-25 19:52:32 +0200

    First files

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+/* Show menu */
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* Remove Mobile menu */
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(e => e.addEventListener('click', linkAction));
+
+/* Change background header */
+
+function scrollHeader(){
+    const header = document.getElementById('header');
+    if(this.scrollY >=50)  header.classList.add('scroll-header')
+    else    header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
